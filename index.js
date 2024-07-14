dc9f026f5218930e2ed5e8f437dddd325d55e27b
dino004 <dmytro4lytvyn@gmail.com>
2024-07-14 12:17:19 +0300

    edit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Meniy the best!");
+import users from "./users.js";
+import { getCurrentMonth } from "./dateFunctions/index.js";
+
+console.log(users);
+console.log(getCurrentMonth());
+
+// const nodemon = require("nodemon");
+
+// const users = require("./users");
+// console.log(users);
+
+// const { clients } = require("./users");
+// console.log(clients);
+
+// const { getCurrentMonth } = require("./dateFunctions");
+// console.log(getCurrentMonth())
+
+// const currentMonth = require('./dateFunctions').getCurrentMonth()
+
+// console.log(currentMonth)
---
+++ users.js
@@ -0,0 +1,10 @@
+const admins = ['Alex', 'Andrey', 'Dmytro'];
+
+const clients = ['Anna', 'Alina', 'Jessica'];
+
+const users = {
+  admins,
+  clients,
+};
+
+export default users;
